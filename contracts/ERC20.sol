674b99d8835d3660cb040fdf1519778f9bcef5e2
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2022-04-01 16:11:48 -0700

    add internal functions

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -73,42 +73,17 @@ contract ERC20 {
      * ----------------
      */
     
-    function transfer(address to, uint256 amount) public virtual returns (bool success) {
-        assembly {
-            let bal := sload(add(caller(), 0x1001)) // load balance of sender
-            if eq(lt(bal, amount), 1) { revert(0, 0) } // check sender has enough tokens
-            sstore(add(caller(), 0x1001), sub(bal, amount)) // subtract amount from sender balance
-            sstore(add(to, 0x1001), add(sload(add(to, 0x1001)), amount)) // add amount to recipient balance
-
-            mstore(0, amount) // store non-indexed approval event parameter
-            log3(
-                0, 0x20, // non-indexed parameter memory slot
-                0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef, // transfer event signature
-                caller(), to // indexed event parameters
-            )
-
-            success := true // return true
-        }
+    function transfer(address to, uint256 amount) public virtual returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
     }
 
     function approve(address spender, uint256 amount) public virtual returns (bool success) {
-        assembly {
-            mstore(0, caller())
-            mstore(0x20, spender)
-            sstore(keccak256(0, 0x40), amount) // write amount at storage slot of approval
-
-            mstore(0, amount) // store non-indexed approval event parameter
-            log3(
-                0, 0x20, // non-indexed parameter memory slot
-                0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925, // approval event signature
-                caller(), spender // indexed event parameters
-            )
-
-            success := true // return true
-        }
+        _approve(msg.sender, spender, amount);
+        return true;
     }
 
-    function transferFrom(address from, address to, uint256 amount) public virtual returns (bool success) {
+    function transferFrom(address from, address to, uint256 amount) public virtual returns (bool) {
         assembly {
             mstore(0, from)
             mstore(0x20, to)
@@ -117,7 +92,54 @@ contract ERC20 {
 
             if eq(lt(approved, amount), 1) { revert(0, 0) } // check spender has enough allowance
             sstore(approval, sub(approved, amount)) // subtract spent from approval
-            sstore(add(from, 0x1001), sub(sload(add(from, 0x1001)), amount)) // subtract amount from sender balance
+        }
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool success) {
+        uint256 newApproved;
+        assembly {
+            mstore(0, caller())
+            mstore(0x20, spender)
+            let approval := keccak256(0, 0x40) // get storage slot of approval
+            let approved := sload(approval) // load approved amount
+
+            newApproved := add(approved, addedValue) // calculate new allowance
+            if iszero(gt(newApproved, approved)) { revert(0, 0) } // check addition overflow
+            sstore(approval, newApproved) // write new approval amount to storage
+        }
+        _approveEvent(msg.sender, spender, newApproved);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool success) {
+        uint256 newApproved;
+        assembly {
+            mstore(0, caller())
+            mstore(0x20, spender)
+            let approval := keccak256(0, 0x40) // get storage slot of approval
+            let approved := sload(approval) // load approved amount
+
+            newApproved := sub(approved, subtractedValue) // calculate new allowance
+            if iszero(lt(newApproved, approved)) { revert(0, 0) } // check subtraction underflow
+            sstore(approval, newApproved) // write new approval amount to storage
+        }
+        _approveEvent(msg.sender, spender, newApproved);
+        return true;
+    }
+
+    /*
+     * ------------------
+     * INTERNAL FUNCTIONS
+     * ------------------
+     */
+    
+    function _transfer(address from, address to, uint256 amount) internal virtual {
+        assembly {
+            let bal := sload(add(from, 0x1001)) // load balance of sender
+            if eq(lt(bal, amount), 1) { revert(0, 0) } // check sender has enough tokens
+            sstore(add(from, 0x1001), sub(bal, amount)) // subtract amount from sender balance
             sstore(add(to, 0x1001), add(sload(add(to, 0x1001)), amount)) // add amount to recipient balance
 
             mstore(0, amount) // store non-indexed approval event parameter
@@ -126,52 +148,26 @@ contract ERC20 {
                 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef, // transfer event signature
                 from, to // indexed event parameters
             )
-
-            success := true // return true
         }
     }
 
-    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool success) {
+    function _approve(address owner, address spender, uint256 amount) internal virtual {
         assembly {
-            mstore(0, caller())
+            mstore(0, owner)
             mstore(0x20, spender)
-            let approval := keccak256(0, 0x40) // get storage slot of approval
-            let approved := sload(approval) // load approved amount
-
-            let newApproved := add(approved, addedValue) // calculate new allowance
-            if iszero(gt(newApproved, approved)) { revert(0, 0) } // check addition overflow
-            sstore(approval, newApproved) // write new approval amount to storage
-
-            mstore(0, newApproved) // store non-indexed approval event parameter
-            log3(
-                0, 0x20, // non-indexed parameter memory slot
-                0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925, // approval event signature
-                caller(), spender // indexed event parameters
-            )
-
-            success := true // return true
+            sstore(keccak256(0, 0x40), amount) // write amount at storage slot of approval
         }
+        _approveEvent(owner, spender, amount);
     }
 
-    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool success) {
+    function _approveEvent(address owner, address spender, uint256 amount) internal virtual {
         assembly {
-            mstore(0, caller())
-            mstore(0x20, spender)
-            let approval := keccak256(0, 0x40) // get storage slot of approval
-            let approved := sload(approval) // load approved amount
-
-            let newApproved := sub(approved, subtractedValue) // calculate new allowance
-            if iszero(lt(newApproved, approved)) { revert(0, 0) } // check subtraction underflow
-            sstore(approval, newApproved) // write new approval amount to storage
-
-            mstore(0, newApproved) // store non-indexed approval event parameter
+            mstore(0, amount) // store non-indexed approval event parameter
             log3(
                 0, 0x20, // non-indexed parameter memory slot
                 0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925, // approval event signature
-                caller(), spender // indexed event parameters
+                owner, spender // indexed event parameters
             )
-
-            success := true // return true
         }
     }
 
