4257ca34806a7199ec4a323a526b826fed2cb747
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2022-04-01 15:14:18 -0700

    add mint

--- contracts/TestERC20.sol
+++ contracts/TestERC20.sol
@@ -7,5 +7,10 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TestERC20 is ERC20("Standard Token", "STANDARD") {
+contract TestERC20 is ERC20("Standard Token", "TOKEN") {
+
+    constructor() {
+        _mint(msg.sender, 10 ** 6 * 1e18);
+    }
+
 }
